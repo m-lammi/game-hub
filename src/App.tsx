e54afa173cf40921df9c153f7631e830b28310ce
Mike <mr.lammi88@gmail.com>
2024-01-18 22:34:58 +1000

    Create basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorScheme="blue" variant="ghost">
-      Button
-    </Button>
+    <Grid
+      templateAreas={{
+        base: `"navbar" "main"`, // <= 1024px no sidepanel
+        lg: `"navbar navbar" "sidepanel main"`, // > 1024px
+      }}
+    >
+      <GridItem area="navbar" bg="coral">
+        Navbar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="sidepanel" bg="grey">
+          Sidepanel
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
